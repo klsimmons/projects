7e9089dcd1c7264c96737dc7a3d40824fbbf73b3
Omnilord <omnilord@privateerconsulting.com>
2018-02-27 23:20:12 -0500

    Refactor for some best practices, adding next method to Player class.  Needs next method implementation on Obstacle class.

--- runner.rb
+++ runner.rb
@@ -2,24 +2,43 @@
 # 20 X 5 board
 T = 0.3
 
-AIR = "*"
-OBSTACLE = "0"
-GROUND = "_"
+AIR = '*'.freeze
+OBSTACLE = '0'.freeze
+GROUND = '_'.freeze
+PLAYER = '@'.freeze
 
 class Player
+  attr_accessor :moving
   attr_reader :position, :avatar
 
-  def initialize(avatar = "@")
+  def initialize(board, avatar = PLAYER)
+    @board = board
     @position = 82
     @avatar = avatar
+    @moving = -1
+
+    @board.place(@avatar, @position)
   end
 
   def move_up
-      @position = @position - 20
+    @position = @position - 20
   end
 
   def move_down
     @position = @position + 20
+  end
+
+  def next
+    if [0, 1].include?(@moving)
+      @board.move(@position, move_up)
+      @moving += 1
+    elsif [2, 3].include?(@moving)
+      @board.move(@position, move_down)
+      @moving += 1
+    else
+      @moving = -1
+      # @board.move(@position, @position += 1)
+    end
   end
 end
 
@@ -27,6 +46,15 @@ class Board
   def initialize
     @size = 100
     @board = 80.times.map { Tile.new(AIR) } + 20.times.map { Tile.new(GROUND) }
+    @board.freeze
+  end
+
+  def [](i)
+    @board[i]
+  end
+
+  def []=(i, v)
+    @board[i] = v
   end
 
   def place(item, index)
@@ -35,7 +63,7 @@ class Board
 
   def draw
     system 'clear' or system 'cls'
-    @board.each_slice(20) { |tiles| puts tiles.join(" ") }
+    @board.each_slice(20) { |tiles| puts tiles.join(' ') }
     puts ''
   end
 
@@ -46,7 +74,7 @@ class Board
 end
 
 class Tile
-attr_accessor :content
+  attr_accessor :content
 
   def initialize(type)
     @type = type
@@ -84,24 +112,33 @@ def jump(board, player)
   end
 end
 
+def jump2(board, player)
+  player.moving = 0
+  10.times do
+    sleep(T)
+    player.next
+    board.draw
+  end
+end
+
 def scroll(board, obstacle)
   19.times do
     sleep(T)
     board.move(obstacle.position, obstacle.scroll)
     board.draw
   end
-  obstacle.position.content = nil
+  board[obstacle.position].content = nil
   board.draw
 end
 
-player = Player.new
 board = Board.new
-obstacle = Obstacle.new
+player = Player.new(board)
+obstacle = Obstacle.new #(board)
 board.place(obstacle.avatar, obstacle.position)
-board.place(player.avatar, player.position)
 board.draw
 #jump(board, player)
-scroll(board, obstacle)
+jump2(board,player)
+#scroll(board, obstacle)
 
 
 
